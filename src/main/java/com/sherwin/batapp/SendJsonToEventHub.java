ddcde94904e445dba4b904ee07a30675b62df0fc
krk4j <145864948+krk4j@users.noreply.github.com>
2024-09-14 15:15:54 -0400

    Update SendJsonToEventHub.java

--- src/main/java/com/sherwin/batapp/SendJsonToEventHub.java
+++ src/main/java/com/sherwin/batapp/SendJsonToEventHub.java
@@ -1,54 +1,17 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpOutputMessage;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class MyCustomListConverter extends AbstractHttpMessageConverter<List<MyRequest>> {
-
-    private final ObjectMapper objectMapper;
-
-    public MyCustomListConverter() {
-        super(MediaType.APPLICATION_JSON);
-        this.objectMapper = new ObjectMapper();
-    }
-
-    @Override
-    protected boolean supports(Class<?> clazz) {
-        return List.class.isAssignableFrom(clazz);
-    }
-
-    @Override
-    protected List<MyRequest> readInternal(Class<? extends List<MyRequest>> clazz, HttpInputMessage inputMessage) throws IOException {
-        return objectMapper.readValue(inputMessage.getBody(), objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, MyRequest.class));
-    }
-
-    @Override
-    protected void writeInternal(List<MyRequest> myRequests, HttpOutputMessage outputMessage) throws IOException {
-        objectMapper.writeValue(outputMessage.getBody(), myRequests);
-    }
-}
-
-
-
-
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MyCustomListConverter());
-    }
-}
+<build>
+    <plugins>
+        <plugin>
+            <groupId>org.codehaus.mojo</groupId>
+            <artifactId>exec-maven-plugin</artifactId>
+            <version>3.0.0</version>
+            <configuration>
+                <mainClass>com.example.MainClass</mainClass>
+                <arguments>
+                    <argument>-javaagent:/path/to/opentelemetry-javaagent.jar</argument>
+                    <argument>-Dotel.exporter.otlp.endpoint=http://localhost:4317</argument>
+                    <argument>-Dotel.resource.attributes=service.name=my-service</argument>
+                </arguments>
+            </configuration>
+        </plugin>
+    </plugins>
+</build>
